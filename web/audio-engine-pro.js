366fbf50688d671a135608974a191f68f122c037
Vijaya <vijayam1030@gmail.com>
2026-01-12 23:54:30 -0500

    even better quality

--- web/audio-engine-pro.js
+++ web/audio-engine-pro.js
@@ -9,45 +9,44 @@ class AudioEnginePro {
         this.effects = {};
         // High-quality sampled instruments (lazy-loaded)
         this.sampleSets = {
+            // Sources: tonejs-instruments (high-quality multisamples; MP3 at 44.1kHz)
             'Piano': {
                 type: 'sampler',
                 options: {
-                    // Salamander Grand Piano multi-sample set (Tone.js hosted)
                     urls: {
-                        'A0': 'A0.mp3',
-                        'C1': 'C1.mp3',
-                        'D#1': 'Ds1.mp3',
-                        'F#1': 'Fs1.mp3',
-                        'A1': 'A1.mp3',
-                        'C2': 'C2.mp3',
-                        'D#2': 'Ds2.mp3',
-                        'F#2': 'Fs2.mp3',
-                        'A2': 'A2.mp3',
-                        'C3': 'C3.mp3',
-                        'D#3': 'Ds3.mp3',
-                        'F#3': 'Fs3.mp3',
-                        'A3': 'A3.mp3',
-                        'C4': 'C4.mp3',
-                        'D#4': 'Ds4.mp3',
-                        'F#4': 'Fs4.mp3',
-                        'A4': 'A4.mp3',
-                        'C5': 'C5.mp3',
-                        'D#5': 'Ds5.mp3',
-                        'F#5': 'Fs5.mp3',
-                        'A5': 'A5.mp3',
-                        'C6': 'C6.mp3',
-                        'D#6': 'Ds6.mp3',
-                        'F#6': 'Fs6.mp3',
-                        'A6': 'A6.mp3',
-                        'C7': 'C7.mp3',
-                        'D#7': 'Ds7.mp3',
-                        'F#7': 'Fs7.mp3',
-                        'A7': 'A7.mp3',
-                        'C8': 'C8.mp3'
+                        'A0': 'A0.mp3', 'C1': 'C1.mp3', 'D#1': 'Ds1.mp3', 'F#1': 'Fs1.mp3',
+                        'A1': 'A1.mp3', 'C2': 'C2.mp3', 'D#2': 'Ds2.mp3', 'F#2': 'Fs2.mp3',
+                        'A2': 'A2.mp3', 'C3': 'C3.mp3', 'D#3': 'Ds3.mp3', 'F#3': 'Fs3.mp3',
+                        'A3': 'A3.mp3', 'C4': 'C4.mp3', 'D#4': 'Ds4.mp3', 'F#4': 'Fs4.mp3',
+                        'A4': 'A4.mp3', 'C5': 'C5.mp3', 'D#5': 'Ds5.mp3', 'F#5': 'Fs5.mp3',
+                        'A5': 'A5.mp3', 'C6': 'C6.mp3'
                     },
-                    release: 1.5,
-                    attack: 0.002,
-                    baseUrl: 'https://tonejs.github.io/audio/salamander/'
+                    release: 1.8,
+                    attack: 0.003,
+                    baseUrl: 'https://cdn.jsdelivr.net/gh/nbrosowsky/tonejs-instruments@master/samples/piano/'
+                }
+            },
+            'Guitar': {
+                type: 'sampler',
+                options: {
+                    urls: {
+                        'E2': 'E2.mp3', 'A2': 'A2.mp3', 'D3': 'D3.mp3', 'G3': 'G3.mp3', 'B3': 'B3.mp3', 'E4': 'E4.mp3'
+                    },
+                    release: 1.2,
+                    attack: 0.005,
+                    baseUrl: 'https://cdn.jsdelivr.net/gh/nbrosowsky/tonejs-instruments@master/samples/guitar-acoustic/'
+                }
+            },
+            'Strings': {
+                type: 'sampler',
+                options: {
+                    urls: {
+                        'C2': 'C2.mp3', 'F2': 'F2.mp3', 'A2': 'A2.mp3', 'C3': 'C3.mp3', 'F3': 'F3.mp3', 'A3': 'A3.mp3',
+                        'C4': 'C4.mp3', 'F4': 'F4.mp3', 'A4': 'A4.mp3', 'C5': 'C5.mp3'
+                    },
+                    release: 2.5,
+                    attack: 0.08,
+                    baseUrl: 'https://cdn.jsdelivr.net/gh/nbrosowsky/tonejs-instruments@master/samples/strings/'
                 }
             }
         };
